36f5ad55f52a23b8e1587aec896984e3c4abf0bf
Dang Quoc Huy <huysamdua@yahoo.com>
2018-07-05 13:30:15 +0700

    Scaling Images to Preserve Aspect Ratio

--- LoveInASnap/ViewController.swift
+++ LoveInASnap/ViewController.swift
@@ -141,3 +141,26 @@ extension ViewController: UIImagePickerControllerDelegate {
     present(imagePickerActionSheet, animated: true)
   }
 }
+
+// MARK: - UIImage extension
+extension UIImage {
+  func scaleImage(_ maxDimension: CGFloat) -> UIImage? {
+    
+    var scaledSize = CGSize(width: maxDimension, height: maxDimension)
+    
+    if size.width > size.height {
+      let scaleFactor = size.height / size.width
+      scaledSize.height = scaledSize.width * scaleFactor
+    } else {
+      let scaleFactor = size.width / size.height
+      scaledSize.width = scaledSize.height * scaleFactor
+    }
+    
+    UIGraphicsBeginImageContext(scaledSize)
+    draw(in: CGRect(origin: .zero, size: scaledSize))
+    let scaledImage = UIGraphicsGetImageFromCurrentImageContext()
+    UIGraphicsEndImageContext()
+    
+    return scaledImage
+  }
+}
